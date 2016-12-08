263d669ed0bc6903762651aba2e8d5aa20b6a7d8
Aatish Sai <meatish.sai@gmail.com>
2016-12-08 21:04:23 +0545

    adding list of hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -26,3 +26,13 @@ export class Hero{
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  {id: 1, name: 'one'},
+  {id: 2, name: 'two'},
+  {id: 3, name: 'three'},
+  {id: 4, name: 'four'},
+  {id: 5, name: 'five'},
+  {id: 6, name: 'six'},
+  {id: 7, name: 'seven'}
+]
